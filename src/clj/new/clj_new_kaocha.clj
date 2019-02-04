5d813d4b482378c01f37fe332761857700554876
Jérémy Van de Wyngaert <jeremyvdw@protonmail.com>
2019-02-05 00:07:12 +0100

    fix project structure and README

--- src/clj/new/clj_new_kaocha.clj
+++ src/clj/new/clj_new_kaocha.clj
@@ -1,15 +1,15 @@
 (ns clj.new.clj-new-kaocha
-  (:require [clj.new.templates :refer [renderer project-name name-to-path ->files sanitize-ns]]))
+  (:require [clj.new.templates :refer [renderer group-name name-to-path ->files sanitize-ns]]))
 
 (def render (renderer "clj_new_kaocha"))
 
 (defn clj-new-kaocha
   "FIXME: write documentation"
-  [name]
-  (let [data {:name (project-name name)
-              :sanitized (name-to-path name)
-              :sanitized-ns (sanitize-ns name)}]
-    (println "Generating fresh 'clj new' kaocha project.")
+  [target]
+  (let [data {:name (group-name target)
+              :sanitized (name-to-path target)
+              :sanitized-ns (sanitize-ns target)}]
+    (println "Generating fresh 'clj new' kaocha project in" (:name data))
     (->files data
              ["deps.edn" (render "deps.edn" data)]
              ["src/{{sanitized}}.clj" (render "core.clj" data)]
